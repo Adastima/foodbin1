0d6415363079edc9038c23961577245b073a206b
Adel <adastima@gmail.com>
2020-10-29 09:42:05 +0800

    select option

--- foodbin1/RestaurantTableViewController.swift
+++ foodbin1/RestaurantTableViewController.swift
@@ -48,51 +48,28 @@ class RestaurantTableViewController: UITableViewController {
         
               return cell
     }
-    //hello
+    
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    // Create an option menu as an action sheet
+    let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
+    
+    // Add Call action
+    let callActionHandler = { (action:UIAlertAction!) -> Void in
+    let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .alert)
+    alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+    self.present(alertMessage, animated: true, completion: nil)
     }
-    */
+    let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)", style: .default, handler: callActionHandler)
+    optionMenu.addAction(callAction)
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+
+    // Add actions to the menu
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+    optionMenu.addAction(cancelAction)
+
+    // Display the menu
+    present(optionMenu, animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
